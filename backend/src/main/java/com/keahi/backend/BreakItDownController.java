aa2462df92163701c8b769c9a97f7d744dfb4c68
kkeahi <trenstonricks@gmail.com>
2025-08-28 20:39:23 -1000

    Working Gemini API query

--- backend/src/main/java/com/keahi/backend/BreakItDownController.java
+++ backend/src/main/java/com/keahi/backend/BreakItDownController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import com.google.gson.Gson;
 
 // ai api request
@@ -13,10 +16,19 @@ import com.google.gson.Gson;
 
 @RestController
 class BreakItDownController {
+
   @PostMapping("/breakitdown")
   public String createPrompt(@RequestBody String context) {
     Gson gson = new Gson();
     Prompt prompt = gson.fromJson(context, Prompt.class);
-    return prompt.getSubject();
+
+    List<String> contextList = new ArrayList<String>();
+    contextList.add("a");
+    contextList.add("b");
+
+    QueryGemini query = new QueryGemini();
+    String response = query.input(contextList);
+    // ai api call
+    return response;
   }
 }
